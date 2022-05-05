b8d4c96bba3de7a8f447cd6b61bc6b0438a3794d
xubo223 <xubo23@meituan.com>
2022-05-05 12:59:11 +0800

    滑动窗口

--- src/array/滑动窗口/无重复字符的最长子串.java
+++ src/array/滑动窗口/无重复字符的最长子串.java
@@ -7,32 +7,24 @@ import java.util.Map;
  */
 public class 无重复字符的最长子串 {
 
-    private static int hash[] = new int[128];
-
     public static int lengthOfLongestSubstring(String s) {
         if("".equals(s)) return 0;
         int start = 0;
         int end = 0;
-        int maxlength = 0;
+        int maxlength = 1;
+        int hash[] = new int[128];
         String window = s.substring(start, end + 1);
         for (char c : window.toCharArray()) hash[c]++;
-        while (end < s.length()) {
-            while (isUnique() && end < s.length()) {
-                maxlength = Math.max(end - start + 1, maxlength);
+        while (end + 1 < s.length()) {
+            while (end + 1 < s.length() && hash[s.charAt(end + 1)] == 0) {
                 end++;
-                if (end < s.length()) hash[s.charAt(end)]++;
+                maxlength = Math.max(end - start + 1, maxlength);
+                hash[s.charAt(end)]++;
             }
             hash[s.charAt(start)]--;
             start++;
         }
         return maxlength;
-    }
-
-    public static boolean isUnique() {
-        for (int i : hash) {
-            if (i > 1) return false;
-        }
-        return true;
     }
 
     public static void main(String[] args) {
